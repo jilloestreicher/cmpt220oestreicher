8352d479a79f70bf6ea3bec3119b776f23dad4ae
Jillian Oestreicher <jillianoestreicher@148-100-158-205.FoxNet.marist.edu>
2017-01-25 15:19:41 -0500

    Project 0 work

---
+++ Projects/0/Driver_prj0.java
@@ -0,0 +1,32 @@
+/**
+
+  * Driver_prj0.java 
+  
+  * Jill Oestreicher
+  
+  * CMPT 220
+  
+  * project 0
+  
+  * January 26, 2017
+  
+  * 1.8
+  
+  * This file contains the output of "Hello World!"
+
+*/
+
+/**
+
+  * Driver_prj0
+
+  *This class prints "Hello World!" to screen
+  
+*/
+public class Driver_prj0 {
+  public static void main(String[] args) { 
+  
+    System.out.println("Hello World!");
+    
+  }
+}
