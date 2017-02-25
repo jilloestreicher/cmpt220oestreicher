4ea1f64411358c98b3b221249a23d4a32678d4fb
jilloestreicher <jillian.oestreicher1@marist.edu>
2017-02-25 00:48:51 -0500

    Update Ex5_1.java

--- Labs/3/Ex5_1.java
+++ Labs/3/Ex5_1.java
@@ -1,30 +1,19 @@
 /**
-
   * Ex5_1.java
-  
   * Jill Oestreicher
-  
   * CMPT 220
-  
   * Lab 3
-  
   * February 23, 2017
-  
   * 1.8
-  
-  * This file contains problem 5.1 of Lab 3
-  
+  * This file contains problem 5.1 of Lab 3 
 */
 
 import java.util.Scanner;
 
 /**
-
   * Ex5_1
-  
   * Reads an unspecified amount of integers, determines amount of positives and negative,
     computes the total and average
-
 */
 
 public class Ex5_1 {
@@ -43,7 +32,7 @@ public class Ex5_1 {
     //collects sum
     //if loop to determine if negative or positive and accumulates for either
     do {
-    
+   
       sum = sum + num;  
       
       count++;
@@ -53,8 +42,8 @@ public class Ex5_1 {
         
       if(num < 0)
         negatives++;
-      
     }
+   
     while((num = s.nextInt()) != 0);
     
     //average takes total and divides by count
@@ -64,6 +53,5 @@ public class Ex5_1 {
     System.out.println("The number of negatives is " + negatives);
     System.out.println("The total is " + sum);
     System.out.println("The average is " + avg);
-    
   }
 }
