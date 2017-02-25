24d6341bd0a2a91aef029196369043a57688f242
jilloestreicher <jillian.oestreicher1@marist.edu>
2017-02-25 00:44:30 -0500

    Update Ex4_5.java

--- Labs/2/Ex4_5.java
+++ Labs/2/Ex4_5.java
@@ -1,29 +1,18 @@
 /**
-  
   * Ex4_5.java
-  
   * Jill Oestreicher
-  
   * CMPT 220
-  
   * Lab 2
-  
   * February 9, 2017
-  
   * 1.8
-  
   * This file contains problem 4.5 of Lab 2
-  
 */
 
 import java.util.Scanner;
 
 /**
-
   * Ex4_5
-  
   * Computes the area of the polygon
-
 */
 
 public class Ex4_5 {
