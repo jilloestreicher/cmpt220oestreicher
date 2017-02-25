579bc9fb4b96ddda6909c114e912ab8aa11e7ea6
jilloestreicher <jillian.oestreicher1@marist.edu>
2017-02-25 00:52:34 -0500

    Update Ex5_7.java

--- Labs/3/Ex5_7.java
+++ Labs/3/Ex5_7.java
@@ -1,27 +1,16 @@
 /**
-
   * Ex5_7.java
-  
   * Jill Oestreicher
-  
   * CMPT 220
-  
   * Lab 3
-  
   * February 23, 2017
-  
   * 1.8
-  
-  * This file contains problem 5.7 of Lab 3
-  
+  * This file contains problem 5.7 of Lab 3 
 */
 
 /**
-
   * Ex5_7
-  
   * Calculates tuition after 10 years and total cost of four years
-  
 */
 
 public class Ex5_7 {
@@ -32,20 +21,14 @@ public class Ex5_7 {
     
     //for loop to accumulate tuition between 0 and 9 years inclusive (10 years)
     for(int i = 0; i < 10; i++) {
-      
       tuition = (tuition * .05) + tuition;
-      
     }  
     
     //for loop to accumulat four year cost by adding tuition four times
     for(int j = 0; j < 4; j++) {
-      
       fourYearCost = fourYearCost + tuition;
-      
     }
-    
     System.out.println("The tuition of this school in 10 years will be " + tuition);
     System.out.println("The total cost of four years' worth of tuition after ten years is " + fourYearCost);
-    
   }
 }
